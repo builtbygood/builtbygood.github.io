94b3c75322511a0625e293efb8723146543233fc
Pete Katsaboulas <pete@virality.me>
2016-07-16 10:10:18 -0500

    add work inquiry modal

--- good-new/js/main.js
+++ good-new/js/main.js
@@ -23,6 +23,52 @@ $(function(){
 
         }
     }
+
+    $('.input').on('click', function(){
+
+        if( !$(this).find('.dropdown').is(':visible') ){
+
+            $(this).find('.dropdown').fadeIn('fast');
+
+        }
+
+    });
+
+    $('.dropdown').each(function(){
+
+        $(this).css({ marginTop: - $(this).outerHeight()/2 + 'px' });
+
+    });
+
+    $('.dropdown li').on('click', function(){
+
+        $(this).parent().parent().parent().find('.display').html($(this).text());
+        $(this).parent().parent().parent().find('.dropdown').delay(250).fadeOut('fast');
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
+    $(document).on("mousedown", function(e) {
+
+        var clicked = $(e.target);  
+        if (  $('.dropdown').is(':visible') && !clicked.is('.dropdown li') ) {
+            $('.dropdown').hide();
+        }
+
+
+    });
+
+    $('.modal-close-btn').on('click', function(){
+
+        $('.modal').fadeOut('fast');
+
+    });
+
+    $('.hire-us-btn').on('click', function(){
+        $('.modal').fadeIn('fast');
+    });
+
 	
 	
 });
