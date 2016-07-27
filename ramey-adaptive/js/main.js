46c135b9235b4c884af70f3f0dfd1742226a9f97
Pete Katsaboulas <pete@virality.me>
2016-07-27 14:21:03 -0500

    add ramey test link

---
+++ ramey-adaptive/js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    var $window = $(window);
+   
+    $('.capabilitiesModal').hide();
+    
+    $('.modalClose').on('click', function(){
+        $(this).parent().fadeOut('fast');
+    });
+
+    $('.open-btn').on('click', function(){
+        var heading     = $(this).parent().find('h3').text();
+
+        $(this).parent().parent().find('.capabilitiesModal h3').html(heading);
+        $(this).parent().parent().find('.capabilitiesModal').fadeIn('fast');
+    });
+	
+	
+});
+
+
+
