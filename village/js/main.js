bfbbaa5b29e5ead65fcba97bbfa9e7973bad9a28
Pete Katsaboulas <pete@virality.me>
2016-09-21 14:52:07 -0500

    responsive styles

--- village/js/main.js
+++ village/js/main.js
@@ -62,7 +62,6 @@ $(function(){
     };
     
 
-
     var scrollTime = 1.2;
     var scrollDistance = 100
 
@@ -94,6 +93,68 @@ $(function(){
         });
 
     });
+
+
+    ajaxMailChimpForm($("#subscribe-form"), $("#subscribe-result"));
+        
+        function ajaxMailChimpForm($form, $resultElement){
+            
+            $form.submit(function(e) {
+
+                e.preventDefault();
+
+                 submitSubscribeForm($form, $resultElement);
+
+                $('.loader').css('opacity', '1');
+    
+            });
+
+        }
+       
+        function submitSubscribeForm($form, $resultElement) {
+
+            $.ajax({
+                type: "GET",
+                url: $form.attr("action"),
+                data: $form.serialize(),
+                cache: false,
+                dataType: "jsonp",
+                jsonp: "c", 
+                contentType: "application/json; charset=utf-8",
+                error: function(error){
+                    
+                },
+                success: function(data){
+                    if (data.result != "success") {
+
+                        var message = data.msg || "Oops! Unable to pre-register. Please try again later.";
+                        
+                        if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
+
+                            message = "Hey! You're already pre-registered.";
+
+                        }
+                        $resultElement.html(message);              
+
+                    } else {
+
+                        $resultElement.html("Thank You! Check your email for confirmation.");
+
+                    }
+
+                    $('.loader').css('opacity', '0');
+                    $('#subscribe-result').fadeIn();
+                  
+                }
+            });
+
+        }
+
+        $("#subscribe-form input").on('focus', function(){
+
+            $('#subscribe-result').fadeOut();
+
+        });
 
 	
 });
